c91d68443bdfb99d1964404447013582d338235e
Jan G <apollg3@gmail.com>
2025-03-16 19:58:55 +0800

    Add Error and LoadingSpinner components for improved error handling and loading states in the application

---
+++ src/app/(web)/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+
+type ErrorProps = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+const Error = ({ error, reset }: ErrorProps) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="container mx-auto">
+      <h2 className="font-heading text-red-800 mb-10">Something went wrong</h2>
+
+      <button className="btn-primary" onClick={() => reset()}>
+        Try again
+      </button>
+    </div>
+  );
+};
+
+export default Error;
